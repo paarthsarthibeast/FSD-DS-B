60299392a33efae7f695afc1778a41d0cd9048fe
paarthsarthibeast <paarthsarthi18@gmail.com>
2024-11-18 10:26:18 +0530

    ok

---
+++ Day10/netflix-clone/src/App.jsx
@@ -0,0 +1,19 @@
+import MovieList from "./MovieList.jsx";
+
+function App() {
+  return (
+    <div>
+      <h1
+        style={{
+          backgroundColor: "black",
+          color: "white",
+          textAlign: "center",
+        }}>
+        Netflix Clone App
+      </h1>
+      <MovieList></MovieList>
+    </div>
+  );
+}
+
+export default App;
---
+++ Day10/netflix-clone/src/Movies.js
@@ -0,0 +1,64 @@
+const movies = [
+    {
+        id:1,
+        image:
+        "https://rukminim2.flixcart.com/image/612/612/xif0q/movie/5/k/z/2023-blu-ray-universal-pictures-english-oppenheimer-original-imagwguyz8cvryfk.jpeg?q=70",
+
+        title: "OPENHEIMER",
+        language: "hindi",
+        cost: "290",
+    },
+
+    {
+        id:2,
+        image:
+        "https://rukminim2.flixcart.com/image/612/612/movie/z/v/4/deadpool-2016-english-dvd-excel-home-videos-original-imaegbwzzyjps635.jpeg?q=70",
+
+        title: "Deadpool",
+        language: "english",
+        cost: "590",
+    },
+
+    {
+        id:3,
+        image:
+        "https://rukminim2.flixcart.com/image/612/612/kl9rssw0/movie/h/2/g/2020-dvd-20th-century-fox-excel-home-videos-english-titanic-original-imagyfz7ct8mqjpw.jpeg?q=70",
+
+        title: "Titanic",
+        language: "hindi",
+        cost: "990",
+    },
+
+    {
+        id:4,
+        image:
+        "https://rukminim2.flixcart.com/image/612/612/movie/v/4/d/morgan-2016-english-blu-ray-excel-home-videos-original-imaepfv5ttgfjpzc.jpeg?q=70",
+
+        title: "morgan",
+        language: "hindi",
+        cost: "690",
+    },
+
+
+    {
+        id:5,
+        image:
+        "https://rukminim2.flixcart.com/image/612/612/jq5iky80/movie/a/a/e/2019-dvd-warner-bros-excel-home-videos-english-the-nun-exclusive-original-imafc7tyfzrpgmwr.jpeg?q=70",
+
+        title: "The Nun",
+        language: "english",
+        cost: "290",
+    },
+
+    {
+        id:6,
+        image:
+        "https://rukminim2.flixcart.com/image/612/612/jgsanww0/movie/g/d/a/2018-dvd-excel-home-videos-english-spider-man-home-coming-dvd-original-imaf4ybdkthsqzjy.jpeg?q=70",
+
+        title: "Spider-man: Home coming",
+        language: "English",
+        cost: "500",
+    },
+]
+
+export default movies;
